ea422d8150c5ccefabd0c295b46bbd29ced94b62
Christian Schulte <cs@schulte.it>
2009-10-06 19:09:09 +0000

    [maven-scm-plugin] Build 2009-10-06T19:08:39+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@772 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/GenerateJavaBundlesCommand.java
+++ src/main/java/org/jomc/cli/commands/GenerateJavaBundlesCommand.java
@@ -180,7 +180,7 @@ import org.jomc.tools.JavaBundles;
  * </ul></p>
  * <p><b>Messages</b><ul>
  * <li>"{@link #getApplicationTitleMessage applicationTitle}"<table>
- * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-6-SNAPSHOT Build 2009-10-06T07:14:40+0000</pre></td></tr>
+ * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-6-SNAPSHOT Build 2009-10-06T19:08:39+0000</pre></td></tr>
  * </table>
  * <li>"{@link #getCannotProcessMessage cannotProcess}"<table>
  * <tr><td valign="top">English:</td><td valign="top"><pre>Cannot process ''{0}'': {1}</pre></td></tr>
@@ -1002,7 +1002,7 @@ public final class GenerateJavaBundlesCommand extends AbstractJomcCommand
     /**
      * Gets the text of the {@code applicationTitle} message.
      * <p><b>Templates</b><br/><table>
-     * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-6-SNAPSHOT Build 2009-10-06T07:14:40+0000</pre></td></tr>
+     * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-6-SNAPSHOT Build 2009-10-06T19:08:39+0000</pre></td></tr>
      * </table></p>
      * @param locale The locale of the message to return.
      * @return The text of the {@code applicationTitle} message.
