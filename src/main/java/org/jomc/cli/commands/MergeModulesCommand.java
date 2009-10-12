c32989c44b1e962e4b3cb48ac3944df6298ac7ab
Christian Schulte <cs@schulte.it>
2009-10-12 13:10:15 +0000

    [maven-scm-plugin] Build 2009-10-12T13:09:43+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@842 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/MergeModulesCommand.java
+++ src/main/java/org/jomc/cli/commands/MergeModulesCommand.java
@@ -166,7 +166,7 @@ import org.jomc.model.Modules;
  * </ul></p>
  * <p><b>Messages</b><ul>
  * <li>"{@link #getApplicationTitleMessage applicationTitle}"<table>
- * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-7-SNAPSHOT Build 2009-10-12T11:11:12+0000</pre></td></tr>
+ * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-7-SNAPSHOT Build 2009-10-12T13:09:43+0000</pre></td></tr>
  * </table>
  * <li>"{@link #getCannotProcessMessage cannotProcess}"<table>
  * <tr><td valign="top">English:</td><td valign="top"><pre>Cannot process ''{0}'': {1}</pre></td></tr>
@@ -879,7 +879,7 @@ public final class MergeModulesCommand extends AbstractJomcCommand
     /**
      * Gets the text of the {@code applicationTitle} message.
      * <p><b>Templates</b><br/><table>
-     * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-7-SNAPSHOT Build 2009-10-12T11:11:12+0000</pre></td></tr>
+     * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-7-SNAPSHOT Build 2009-10-12T13:09:43+0000</pre></td></tr>
      * </table></p>
      * @param locale The locale of the message to return.
      * @return The text of the {@code applicationTitle} message.
