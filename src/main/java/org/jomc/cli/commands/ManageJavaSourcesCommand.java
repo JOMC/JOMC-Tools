aecfdd8a6ef0ad9f6bd56dfe85dc47790c399187
Christian Schulte <cs@schulte.it>
2010-01-19 04:16:22 +0000

    [maven-scm-plugin] Build 2010-01-19T04:15:48+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@1337 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/ManageJavaSourcesCommand.java
+++ src/main/java/org/jomc/cli/commands/ManageJavaSourcesCommand.java
@@ -154,7 +154,7 @@ import org.jomc.tools.JavaSources;
  * </ul></p>
  * <p><b>Messages</b><ul>
  * <li>"{@link #getApplicationTitleMessage applicationTitle}"<table>
- * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-15-SNAPSHOT Build 2010-01-18T05:48:47+0000</pre></td></tr>
+ * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-15-SNAPSHOT Build 2010-01-19T04:15:48+0000</pre></td></tr>
  * </table>
  * <li>"{@link #getCannotProcessMessage cannotProcess}"<table>
  * <tr><td valign="top">English:</td><td valign="top"><pre>Cannot process ''{0}'': {1}</pre></td></tr>
@@ -838,7 +838,7 @@ public final class ManageJavaSourcesCommand extends AbstractJomcCommand
     /**
      * Gets the text of the {@code applicationTitle} message.
      * <p><b>Templates</b><br/><table>
-     * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-15-SNAPSHOT Build 2010-01-18T05:48:47+0000</pre></td></tr>
+     * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-alpha-15-SNAPSHOT Build 2010-01-19T04:15:48+0000</pre></td></tr>
      * </table></p>
      * @param locale The locale of the message to return.
      * @return The text of the {@code applicationTitle} message.
