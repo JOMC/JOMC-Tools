90df54333b7bb63184fc667135492aae9889accf
Christian Schulte <cs@schulte.it>
2010-04-03 18:41:18 +0000

    [maven-scm-plugin] Build 2010-04-03T18:39:42+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@1749 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/ValidateModulesCommand.java
+++ src/main/java/org/jomc/cli/commands/ValidateModulesCommand.java
@@ -99,7 +99,7 @@ import org.jomc.model.ModelValidationReport;
  * </ul></p>
  * <p><b>Messages</b><ul>
  * <li>"{@link #getApplicationTitle applicationTitle}"<table>
- * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-beta-2-SNAPSHOT Build 2010-04-03T18:21:33+0000</pre></td></tr>
+ * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-beta-2-SNAPSHOT Build 2010-04-03T18:39:42+0000</pre></td></tr>
  * </table>
  * <li>"{@link #getCannotProcessMessage cannotProcessMessage}"<table>
  * <tr><td valign="top">English:</td><td valign="top"><pre>Cannot process ''{0}'': {1}</pre></td></tr>
@@ -513,7 +513,7 @@ public class ValidateModulesCommand extends AbstractJomcCommand
     /**
      * Gets the text of the {@code applicationTitle} message.
      * <p><b>Templates</b><br/><table>
-     * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-beta-2-SNAPSHOT Build 2010-04-03T18:21:33+0000</pre></td></tr>
+     * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-beta-2-SNAPSHOT Build 2010-04-03T18:39:42+0000</pre></td></tr>
      * </table></p>
      * @param locale The locale of the message to return.
      * @return The text of the {@code applicationTitle} message.
