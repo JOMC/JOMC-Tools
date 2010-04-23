d7f1e2e1f98181acfadb7dd37b7036d6392e24fd
Christian Schulte <cs@schulte.it>
2010-04-23 00:29:36 +0000

    o Added goals 'attach-main-module' and 'attach-test-module' to the 'maven-jomc-plugin'.
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@1779 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/GenerateResourcesCommand.java
+++ src/main/java/org/jomc/cli/commands/GenerateResourcesCommand.java
@@ -112,7 +112,7 @@ import org.jomc.tools.ResourceFileProcessor;
  * </ul></p>
  * <p><b>Messages</b><ul>
  * <li>"{@link #getApplicationTitle applicationTitle}"<table>
- * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-beta-3-SNAPSHOT Build 2010-04-22T20:10:48+0000</pre></td></tr>
+ * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-beta-3-SNAPSHOT Build 2010-04-23T02:13:25+0200</pre></td></tr>
  * </table>
  * <li>"{@link #getCannotProcessMessage cannotProcessMessage}"<table>
  * <tr><td valign="top">English:</td><td valign="top"><pre>Cannot process ''{0}'': {1}</pre></td></tr>
@@ -645,7 +645,7 @@ public final class GenerateResourcesCommand extends AbstractJomcCommand
     /**
      * Gets the text of the {@code applicationTitle} message.
      * <p><b>Templates</b><br/><table>
-     * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-beta-3-SNAPSHOT Build 2010-04-22T20:10:48+0000</pre></td></tr>
+     * <tr><td valign="top">English:</td><td valign="top"><pre>JOMC Version 1.0-beta-3-SNAPSHOT Build 2010-04-23T02:13:25+0200</pre></td></tr>
      * </table></p>
      * @param locale The locale of the message to return.
      * @return The text of the {@code applicationTitle} message.
