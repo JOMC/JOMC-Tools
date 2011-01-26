400649d3c10bc463e96f7e517df85c05b839ef96
Christian Schulte <cs@schulte.it>
2011-01-26 19:41:49 +0000

    [maven-scm-plugin] Build 2011-01-26T20:36:20+0100
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@3027 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/AbstractModletCommand.java
+++ src/main/java/org/jomc/cli/commands/AbstractModletCommand.java
@@ -208,7 +208,7 @@ import org.jomc.modlet.Services;
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getApplicationTitle applicationTitle}</td>
  *       <td align="left" valign="top" nowrap>English (default)</td>
- *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-01-26T20:13:34+0100</code></pre></td>
+ *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-01-26T20:36:20+0100</code></pre></td>
  *     </tr>
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getClasspathElementInfo classpathElementInfo}</td>
@@ -1194,7 +1194,7 @@ public abstract class AbstractModletCommand extends AbstractCommand
      *     </tr>
      *     <tr class="TableRow">
      *       <td align="left" valign="top" nowrap>English (default)</td>
-     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-01-26T20:13:34+0100</code></pre></td>
+     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-01-26T20:36:20+0100</code></pre></td>
      *     </tr>
      *   </table>
      * </p>
