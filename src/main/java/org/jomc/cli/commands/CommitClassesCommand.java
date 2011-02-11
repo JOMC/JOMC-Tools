a854efd278a475350d6da454ce1aa7c1b3f03c46
Christian Schulte <cs@schulte.it>
2011-02-11 02:52:06 +0000

    [maven-scm-plugin] Build 2011-02-11T03:51:23+0100
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@3135 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/CommitClassesCommand.java
+++ src/main/java/org/jomc/cli/commands/CommitClassesCommand.java
@@ -276,7 +276,7 @@ import org.jomc.tools.ClassFileProcessor;
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getApplicationTitle applicationTitle}</td>
  *       <td align="left" valign="top" nowrap>English (default)</td>
- *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-02-08T19:38:48+0100</code></pre></td>
+ *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-02-11T03:51:23+0100</code></pre></td>
  *     </tr>
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getCannotProcessMessage cannotProcessMessage}</td>
@@ -1304,7 +1304,7 @@ public final class CommitClassesCommand extends AbstractClassFileProcessorComman
      *     </tr>
      *     <tr class="TableRow">
      *       <td align="left" valign="top" nowrap>English (default)</td>
-     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-02-08T19:38:48+0100</code></pre></td>
+     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-02-11T03:51:23+0100</code></pre></td>
      *     </tr>
      *   </table>
      * </p>
--- src/main/java/org/jomc/cli/commands/ValidateClassesCommand.java
+++ src/main/java/org/jomc/cli/commands/ValidateClassesCommand.java
@@ -263,7 +263,7 @@ import org.jomc.tools.ClassFileProcessor;
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getApplicationTitle applicationTitle}</td>
  *       <td align="left" valign="top" nowrap>English (default)</td>
- *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-02-08T19:38:48+0100</code></pre></td>
+ *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-02-11T03:51:23+0100</code></pre></td>
  *     </tr>
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getCannotProcessMessage cannotProcessMessage}</td>
@@ -1207,7 +1207,7 @@ public final class ValidateClassesCommand extends AbstractClassFileProcessorComm
      *     </tr>
      *     <tr class="TableRow">
      *       <td align="left" valign="top" nowrap>English (default)</td>
-     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-02-08T19:38:48+0100</code></pre></td>
+     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-02-11T03:51:23+0100</code></pre></td>
      *     </tr>
      *   </table>
      * </p>
