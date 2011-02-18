8706a5ea3cf11c6629d93b376c035a43c53810a4
Christian Schulte <cs@schulte.it>
2011-02-18 00:33:12 +0000

    o s/(c)/(C)/g
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@3143 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/AbstractModletCommand.java
+++ src/main/java/org/jomc/cli/commands/AbstractModletCommand.java
@@ -1,8 +1,8 @@
 // SECTION-START[License Header]
 // <editor-fold defaultstate="collapsed" desc=" Generated License ">
 /*
- *   Copyright (c) 2009 - 2011 The JOMC Project
- *   Copyright (c) 2005 - 2011 Christian Schulte <schulte2005@users.sourceforge.net>
+ *   Copyright (C) 2009 - 2011 The JOMC Project
+ *   Copyright (C) 2005 - 2011 Christian Schulte <schulte2005@users.sourceforge.net>
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
