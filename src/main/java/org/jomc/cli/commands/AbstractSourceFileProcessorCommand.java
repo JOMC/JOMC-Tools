d02e8a0f6fb5d335ba82a34443a6c2ccb5382979
Christian Schulte <cs@schulte.it>
2011-04-20 18:04:15 +0000

    [maven-scm-plugin] Build 2011-04-20T20:03:26+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@3321 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/AbstractSourceFileProcessorCommand.java
+++ src/main/java/org/jomc/cli/commands/AbstractSourceFileProcessorCommand.java
@@ -277,7 +277,7 @@ import org.jomc.tools.SourceFileProcessor;
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getApplicationTitle applicationTitle}</td>
  *       <td align="left" valign="top" nowrap>English (default)</td>
- *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-20T09:36:03+0200</code></pre></td>
+ *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-20T20:03:26+0200</code></pre></td>
  *     </tr>
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getCannotProcessMessage cannotProcessMessage}</td>
@@ -1056,7 +1056,7 @@ public abstract class AbstractSourceFileProcessorCommand extends AbstractJomcToo
      *     </tr>
      *     <tr class="TableRow">
      *       <td align="left" valign="top" nowrap>English (default)</td>
-     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-20T09:36:03+0200</code></pre></td>
+     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-20T20:03:26+0200</code></pre></td>
      *     </tr>
      *   </table>
      * </p>
--- src/main/java/org/jomc/cli/commands/ManageSourcesCommand.java
+++ src/main/java/org/jomc/cli/commands/ManageSourcesCommand.java
@@ -291,7 +291,7 @@ import org.jomc.tools.SourceFileProcessor;
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getApplicationTitle applicationTitle}</td>
  *       <td align="left" valign="top" nowrap>English (default)</td>
- *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-20T09:36:03+0200</code></pre></td>
+ *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-20T20:03:26+0200</code></pre></td>
  *     </tr>
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getCannotProcessMessage cannotProcessMessage}</td>
@@ -1273,7 +1273,7 @@ public final class ManageSourcesCommand extends AbstractSourceFileProcessorComma
      *     </tr>
      *     <tr class="TableRow">
      *       <td align="left" valign="top" nowrap>English (default)</td>
-     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-20T09:36:03+0200</code></pre></td>
+     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-20T20:03:26+0200</code></pre></td>
      *     </tr>
      *   </table>
      * </p>
