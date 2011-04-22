ea610984f1e7561f07de48d4e22cc8e091194b76
Christian Schulte <cs@schulte.it>
2011-04-22 11:47:32 +0000

    [maven-scm-plugin] Build 2011-04-22T13:46:42+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@3324 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/AbstractCommand.java
+++ src/main/java/org/jomc/cli/commands/AbstractCommand.java
@@ -138,7 +138,7 @@ import org.apache.commons.cli.CommandLine;
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getApplicationTitle applicationTitle}</td>
  *       <td align="left" valign="top" nowrap>English (default)</td>
- *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-20T20:03:26+0200</code></pre></td>
+ *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-22T13:46:42+0200</code></pre></td>
  *     </tr>
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getCommandFailureMessage commandFailureMessage}</td>
@@ -566,7 +566,7 @@ public abstract class AbstractCommand
      *     </tr>
      *     <tr class="TableRow">
      *       <td align="left" valign="top" nowrap>English (default)</td>
-     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-20T20:03:26+0200</code></pre></td>
+     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-22T13:46:42+0200</code></pre></td>
      *     </tr>
      *   </table>
      * </p>
