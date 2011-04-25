b59bc2cb21ffa8d7b99e9eb0082ed443824389c5
Christian Schulte <cs@schulte.it>
2011-04-25 03:56:11 +0000

    [maven-scm-plugin] Build 2011-04-25T05:55:18+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@3330 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/AbstractClassFileProcessorCommand.java
+++ src/main/java/org/jomc/cli/commands/AbstractClassFileProcessorCommand.java
@@ -273,7 +273,7 @@ import org.jomc.tools.ClassFileProcessor;
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getApplicationTitle applicationTitle}</td>
  *       <td align="left" valign="top" nowrap>English (default)</td>
- *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-22T13:46:42+0200</code></pre></td>
+ *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-25T05:55:18+0200</code></pre></td>
  *     </tr>
  *     <tr class="TableRowColor">
  *       <td align="left" valign="top" nowrap>{@link #getCannotProcessMessage cannotProcessMessage}</td>
@@ -1023,7 +1023,7 @@ public abstract class AbstractClassFileProcessorCommand extends AbstractJomcTool
      *     </tr>
      *     <tr class="TableRow">
      *       <td align="left" valign="top" nowrap>English (default)</td>
-     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-22T13:46:42+0200</code></pre></td>
+     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-04-25T05:55:18+0200</code></pre></td>
      *     </tr>
      *   </table>
      * </p>
