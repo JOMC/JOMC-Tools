5d81a8d66617f4b84c66e2b784c37e45fe769ef0
Christian Schulte <cs@schulte.it>
2011-07-26 15:53:14 +0000

    [maven-scm-plugin] Build 2011-07-26T17:51:45+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@3468 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/AbstractSourceFileProcessorCommand.java
+++ src/main/java/org/jomc/cli/commands/AbstractSourceFileProcessorCommand.java
@@ -1090,7 +1090,7 @@ public abstract class AbstractSourceFileProcessorCommand extends AbstractJomcToo
      *     </tr>
      *     <tr class="TableRow">
      *       <td align="left" valign="top" nowrap>English (default)</td>
-     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-07-26T17:28:32+0200</code></pre></td>
+     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-07-26T17:51:45+0200</code></pre></td>
      *     </tr>
      *   </table>
      * </p>
