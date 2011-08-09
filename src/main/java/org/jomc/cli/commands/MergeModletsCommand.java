057e0c4d0a2d79317a649cf0009ec74d732b0e04
Christian Schulte <cs@schulte.it>
2011-08-09 23:28:40 +0000

    [maven-scm-plugin] Build 2011-08-09T23:26:46+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@3574 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/MergeModletsCommand.java
+++ src/main/java/org/jomc/cli/commands/MergeModletsCommand.java
@@ -1228,7 +1228,7 @@ public final class MergeModletsCommand extends AbstractModletCommand
      *     </tr>
      *     <tr class="TableRow">
      *       <td align="left" valign="top" nowrap>English (default)</td>
-     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-08-05T21:52:57+0200</code></pre></td>
+     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-08-09T23:26:46+0000</code></pre></td>
      *     </tr>
      *   </table>
      * </p>
--- src/main/java/org/jomc/cli/commands/ShowModelCommand.java
+++ src/main/java/org/jomc/cli/commands/ShowModelCommand.java
@@ -1090,7 +1090,7 @@ public final class ShowModelCommand extends AbstractModelCommand
      *     </tr>
      *     <tr class="TableRow">
      *       <td align="left" valign="top" nowrap>English (default)</td>
-     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-08-05T21:52:57+0200</code></pre></td>
+     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-08-09T23:26:46+0000</code></pre></td>
      *     </tr>
      *   </table>
      * </p>
