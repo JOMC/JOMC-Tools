616d3ba5c1baaff1eee7079f2e93e1d28a88ed49
Christian Schulte <cs@schulte.it>
2011-08-10 12:30:42 +0000

    [maven-scm-plugin] Build 2011-08-10T12:28:42+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@3583 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/cli/commands/AbstractCommand.java
+++ src/main/java/org/jomc/cli/commands/AbstractCommand.java
@@ -571,7 +571,7 @@ public abstract class AbstractCommand
      *     </tr>
      *     <tr class="TableRow">
      *       <td align="left" valign="top" nowrap>English (default)</td>
-     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-08-09T23:33:22+0000</code></pre></td>
+     *       <td align="left" valign="top" nowrap><pre><code>JOMC CLI Version 1.2-SNAPSHOT Build 2011-08-10T12:28:42+0000</code></pre></td>
      *     </tr>
      *   </table>
      * </p>
