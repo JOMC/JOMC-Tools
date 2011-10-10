205d380ecc0888bc54e96272c0863fa9dc929479
Christian Schulte <cs@schulte.it>
2011-10-10 16:25:09 +0000

    o Re-ordered imports.
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-ant-tasks@3851 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/test/java/org/jomc/ant/test/ManageSourcesTaskTest.java
+++ src/test/java/org/jomc/ant/test/ManageSourcesTaskTest.java
@@ -35,12 +35,12 @@ import org.apache.tools.ant.Project;
 import org.jomc.ant.ManageSourcesTask;
 import org.jomc.ant.SourceProcessingException;
 import org.jomc.ant.test.support.AntExecutionResult;
-import org.junit.Test;
 import static org.jomc.ant.test.support.Assert.assertException;
 import static org.jomc.ant.test.support.Assert.assertExceptionMessage;
 import static org.jomc.ant.test.support.Assert.assertMessageLogged;
 import static org.jomc.ant.test.support.Assert.assertMessageNotLogged;
 import static org.jomc.ant.test.support.Assert.assertNoException;
+import org.junit.Test;
 
 /**
  * Test cases for class {@code org.jomc.ant.ManageSourcesTask}.
