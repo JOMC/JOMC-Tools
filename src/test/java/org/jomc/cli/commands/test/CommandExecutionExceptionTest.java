80589406f01dc5137863c1d5d6d455dbccf6b8d5
Christian Schulte <cs@schulte.it>
2012-02-11 22:53:31 +0000

    [maven-scm-plugin] Build 2012-02-11T22:52:33+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-cli@4231 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/test/java/org/jomc/cli/commands/test/CommandExecutionExceptionTest.java
+++ src/test/java/org/jomc/cli/commands/test/CommandExecutionExceptionTest.java
@@ -1,6 +1,7 @@
 // SECTION-START[License Header]
 // <editor-fold defaultstate="collapsed" desc=" Generated License ">
 /*
+ *   Java Object Management and Configuration
  *   Copyright (C) Christian Schulte, 2012-013
  *   All rights reserved.
  *
