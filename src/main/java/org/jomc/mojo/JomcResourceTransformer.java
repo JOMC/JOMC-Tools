6089eb29913a2d5bec6abdacb3ff2b18fc6d0a11
Christian Schulte <cs@schulte.it>
2014-11-15 19:29:28 +0000

    o Updated the use value of the 'model' attribute of the 'Modlet' complex type of XML target namespace 'http://jomc.org/modlet' from 'optional' to 'required'. o Deprecated all methods required to deal with 'Modlet's not part of a 'Model' now that the XML schema correctly enforces 'Modlet's to always be part of a 'Model'.
    Closes #77
    
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/maven-jomc-plugin@4986 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/mojo/JomcResourceTransformer.java
+++ src/main/java/org/jomc/mojo/JomcResourceTransformer.java
@@ -116,7 +116,7 @@ import org.jomc.modlet.Modlets;
  *   &lt;providerLocation&gt;META-INF/custom-services&lt;/providerLocation&gt;
  *   &lt;platformProviderLocation&gt;${java.home}/jre/lib/custom-jomc.properties&lt;/platformProviderLocation&gt;
  *   &lt;modletLocation&gt;META-INF/custom-jomc-modlet.xml&lt;/modletLocation&gt;
- *   &lt;modletSchemaSystemId&gt;http://custom.host.tld/custom/path/jomc-modlet-1.3.xsd&lt;/modletSchemaSystemId&gt;
+ *   &lt;modletSchemaSystemId&gt;http://custom.host.tld/custom/path/jomc-modlet-1.8.xsd&lt;/modletSchemaSystemId&gt;
  * &lt;/transformer&gt;
  * </pre></p>
  *
